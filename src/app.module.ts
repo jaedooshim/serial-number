319c16a32a04d30abb284d51cdb2ef50c8653c87
shimjaedoo <woen1637@naver.com>
2024-03-29 19:46:00 +0900

    chore : jwt module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
-  imports: [AdminModule, PrismaModule],
+  imports: [AdminModule, PrismaModule, JwtModule],
   controllers: [AppController],
   providers: [AppService],
 })
