53d1a3c93d5bc1bb33da7f12a29eca07dcd61263
shimjaedoo <woen1637@naver.com>
2024-03-29 19:45:21 +0900

    fix : 성공시 토큰값 반환

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { AdminRepository } from './admin.repository';
 import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '../jwt/jwt.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, JwtModule],
   controllers: [AdminController],
   providers: [AdminService, AdminRepository],
 })
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,10 +1,14 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { AdminRepository } from './admin.repository';
 import { ISerialNumber } from './types/request.interface';
+import { JwtService } from '../jwt/jwt.service';
 
 @Injectable()
 export class AdminService {
-  constructor(private adminRepository: AdminRepository) {}
+  constructor(
+    private adminRepository: AdminRepository,
+    private jwtService: JwtService,
+  ) {}
 
   /* 관리자 일련코드 생성 */
   async create(data: ISerialNumber) {
@@ -15,9 +19,8 @@ export class AdminService {
   async compareSerialNumber(serialNumber: number): Promise<string> {
     const compare = await this.adminRepository.findSerialNumber(serialNumber);
     if (!compare) throw new ConflictException('인증번호가 일치하지 않습니다. 확인 후 입력 바랍니다.');
-    return '인증번호가 일치합니다.';
-  }
 
-  // expires 유효시간을 줘서, 코드 일련번호 재사용 금지
-  // 유효시간 설정 로직 추가 예정
+    const accessToken = this.jwtService.sign(compare.id);
+    return accessToken;
+  }
 }
